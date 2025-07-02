7a796718637e04186ad56b176089200d08f9f9a4
safi-sfn <mdsafiruddin24@gmail.com>
2025-07-02 18:50:48 +0530

    adding Question

--- Telusko_AssignmentQuestion/src/main/java/com/safi/assignments/question1/Calculator.java
+++ Telusko_AssignmentQuestion/src/main/java/com/safi/assignments/question1/Calculator.java
@@ -1,5 +1,11 @@
 package main.java.com.safi.assignments.question1;
 
+/*
+ * Basic Calculator: Create a Java program that functions as a basic calculator. 
+ * It should take two numbers and perform operations like addition, subtraction, 
+ * multiplication, and division based on user input.
+ */
+
 public class Calculator {
 
 	public static void main(String[] args) {
